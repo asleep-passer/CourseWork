4d1f96df9160e0e8ac82547c80a7ed8e089eb6df
asleep-passer <1328958046@qq.com>
2026-04-14 19:49:02 +0800

    uadate class road

--- models/Road.py
+++ models/Road.py
@@ -1,4 +1,23 @@
+from enum import Enum
+import pygame as pg
+
+class Direction(Enum):
+    UP=(0,1)
+    DOWN=(0,-1)
+    RIGHT=(1,0)
+    LEFT=(-1,0)
+
+
 class Road:
-    def __init__(self) -> None:
+    class RoadType(Enum):
+        STRAIGHT_ROAD=(Direction.UP,Direction.DOWN)#上下可通行
+        BEND_ROAD=    (Direction.UP,Direction.RIGHT)#上右可通行
+        T_SHAPED_ROAD=(Direction.UP,Direction.RIGHT,Direction.LEFT)#上右左可通行
+        CROSS_ROAD=   (Direction.UP,Direction.DOWN,Direction.RIGHT,Direction.LEFT)#上下右左可通行
+
+    def __init__(self,road_type:'Road.RoadType') -> None:
+        self.passable_direction=road_type.value
+        self.rotated=1
+        self.position=pg.Vector2(0,0)
         print("A Road is created.")
         
