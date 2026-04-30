e855e6eb57006f4ca388948cb395eb898023d36b
asleep-passer <1328958046@qq.com>
2026-04-30 15:07:13 +0800

    FinishRoadModel

--- models/Road.py
+++ models/Road.py
@@ -1,23 +1,49 @@
 from enum import Enum
-import pygame as pg
 
 class Direction(Enum):
-    UP=(0,1)
-    DOWN=(0,-1)
-    RIGHT=(1,0)
-    LEFT=(-1,0)
+    UP=   0
+    RIGHT=1
+    DOWN= 2
+    LEFT= 3
 
-
-class Road:
+class RoadModel:
     class RoadType(Enum):
-        STRAIGHT_ROAD=(Direction.UP,Direction.DOWN)#上下可通行
-        BEND_ROAD=    (Direction.UP,Direction.RIGHT)#上右可通行
-        T_SHAPED_ROAD=(Direction.UP,Direction.RIGHT,Direction.LEFT)#上右左可通行
-        CROSS_ROAD=   (Direction.UP,Direction.DOWN,Direction.RIGHT,Direction.LEFT)#上下右左可通行
+        OBSTACLE_ROAD=0
+        STRAIGHT_ROAD=1
+        BEND_ROAD=    2
+        T_SHAPED_ROAD=3
+        CROSS_ROAD=   4
+        START_ROAD=   5
+        END_ROAD=     6
 
-    def __init__(self,road_type:'Road.RoadType') -> None:
-        self.passable_direction=road_type.value
-        self.rotated=1
-        self.position=pg.Vector2(0,0)
-        print("A Road is created.")
+    def __init__(self,road_type:RoadType) -> None:
+        self.road_type=road_type
+        
+        if road_type==self.RoadType.OBSTACLE_ROAD:
+            self.passable_direction=()
+        elif road_type==self.RoadType.STRAIGHT_ROAD:
+            self.passable_direction=(Direction.UP,Direction.DOWN)
+        elif road_type==self.RoadType.BEND_ROAD:
+            self.passable_direction=(Direction.UP,Direction.RIGHT)
+        elif road_type==self.RoadType.T_SHAPED_ROAD:
+            self.passable_direction=(Direction.UP,Direction.RIGHT,Direction.LEFT)
+        else:
+            self.passable_direction=(Direction.UP,Direction.RIGHT,Direction.DOWN,Direction.LEFT)
+
+        self.rotated=0
+        print(f"A {self.road_type.__str__}is created.")
+
+    def get_passable_direction(self):
+        cur_passable_direction=[]
+        for dir in self.passable_direction:
+            cur_passable_direction.append(Direction((dir.value+self.rotated)%4))
+        return cur_passable_direction
+    
+    def rotate(self):
+        self.rotated+=1
+
+    def reset(self):
+        self.rotated=0
+
+
         
