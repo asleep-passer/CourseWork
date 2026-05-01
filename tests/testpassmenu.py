aa2cc9cf3753db020336987803ec766f315d1da1
asleep-passer <1328958046@qq.com>
2026-05-01 18:16:48 +0800

    passmenu

---
+++ tests/testpassmenu.py
@@ -0,0 +1,50 @@
+from models import NormalRoadListModel as nl
+from models import AdminRoadListModel as al
+from models import Road as rd
+from view import RoadListView as rl
+import pygame as pg
+import config as cg
+from view.passmenu import PassMenuView
+
+def test3():
+    screen=pg.display.set_mode(cg.screenSize)
+    pass_menu = PassMenuView(screen, score=2500, level_id=3, stars=2)
+    running = True
+    clock = pg.time.Clock()
+
+    while running:
+    # 处理事件 (如关闭窗口)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+            action = pass_menu.handle_event(event)
+            if action == 'next_level':
+                pass
+                # 跳转到下一关
+            elif action == 'retry':
+                pass
+                # 重试当前关卡
+
+    # --- 渲染步骤 ---
+    # 1. 清空屏幕 (用白色填充)
+        WHITE = (255, 255, 255)
+        screen.fill(WHITE)
+
+    # 2. 将图片绘制到屏幕上
+    # screen.blit(Surface_to_draw, destination_Rect_or_Coordinates)
+        pass_menu.draw()
+
+    # 3. 更新显示
+        pg.display.flip()
+
+    # 控制帧率
+        clock.tick(60)
+
+    # 退出 Pygame
+    pg.quit()
+
+def main():
+    pg.init()
+    test3()
+
+main()
--- tests/testroadlist.py
+++ tests/testroadlist.py
@@ -1,6 +1,9 @@
 from models import NormalRoadListModel as nl
 from models import AdminRoadListModel as al
 from models import Road as rd
+from view import RoadListView as rl
+import pygame as pg
+import config as cg
 
 def test1():
     print("Test1:")
@@ -27,11 +30,39 @@ def test2():
     if road!=None: road_list.store_road(road)
     print(road_list.get_road_num(rd.RoadType.START_ROAD))
 
+def test3():
+    screen=pg.display.set_mode(cg.screenSize)
+    road_list=rl(nl(1,2,3,4),screen)
+    running = True
+    clock = pg.time.Clock()
 
+    while running:
+    # 处理事件 (如关闭窗口)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+
+    # --- 渲染步骤 ---
+    # 1. 清空屏幕 (用白色填充)
+        WHITE = (255, 255, 255)
+        screen.fill(WHITE)
+
+    # 2. 将图片绘制到屏幕上
+    # screen.blit(Surface_to_draw, destination_Rect_or_Coordinates)
+        road_list.draw()
+
+    # 3. 更新显示
+        pg.display.flip()
+
+    # 控制帧率
+        clock.tick(60)
+
+    # 退出 Pygame
+    pg.quit()
 
 def main():
-    test1()
-    test2()
+    pg.init()
+    test3()
 
 main()
     
